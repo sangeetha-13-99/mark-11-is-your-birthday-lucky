3c29cecf6d8a98a5beff8b11728a160ace325407
Sangeetha Jula <74001727+sangeetha-13-99@users.noreply.github.com>
2022-08-31 01:14:25 +0530

    refactor: adding footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Heading from "./Heading";
 import Main from "./Main";
+import Footer from "./Footer";
 export default function App() {
   return (
     <div className="App">
       <Heading />
       <Main/>
+      <Footer />
     </div>
   );
 }
